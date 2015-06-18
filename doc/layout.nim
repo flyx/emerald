07c2c5c7923620408306c586d76ce2c76795e6bd
Felix Krause <contact@flyx.org>
2015-06-18 21:56:14 +0200

    Added license and improved documentation home

--- doc/layout.nim
+++ doc/layout.nim
@@ -33,7 +33,11 @@ proc layout*(title: string,
                 article:
                     block content: discard
             footer:
-                p: "This documentation has been generated with emerald."
+                p:
+                    "This documentation has been generated with emerald."; br()
+                    """It has intentionally been designed without gradients and
+                    lense flares. If you want gradients and lense flares, go
+                    watch a J. J. Abrahms movie."""
             
             {. filters = nil .}
             """<a href="https://github.com/flyx/emerald"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>"""
--- doc/mainPage.nim
+++ doc/mainPage.nim
@@ -8,33 +8,35 @@ proc home*() {. html_templ: layout .} =
     title = "Home"
     {. debug = true .}
     replace content:
-        p:
-            strong("emerald"); """ is a Nim library that enables you to write
-            HTML templates. It is implemented as domain-specific language that
-            can be used directly in your Nim source code via macros. Features
-            include:"""
-        ul:
-            li: strong("HTML validation"); """: emerald validates your HTML
-                structure when it compiles your template. This validation checks
-                for unknown, ill-placed and missing HTML tags and attributes,
-                but does not implement the whole HTML 5 spec. It is a tool for
-                you to discover errors early."""
-            li: strong("Mixins"); """: You can re-use parts of your template
-                code by placing it in mixins and calling the mixin from the
-                template. emerald is able to check the whole resulting HTML
-                structure."""
-            li: strong("Filtering"); """: By default, emerald converts special
-                HTML characters to their corresponding entities, but you can
-                customize the whole filter chain and also write your own
-                filters."""
-            li: strong("Inheritance"); """: Templates can inherit from other
-                templates. You can define your base structure in a master
-                template, and add content with child templates. emerald is still
-                able to check the whole resulting HTML structure."""
-        d:
-            {. filters = pygmentize("nim") .}
-            {. preserve_whitespace = true .}
-            """
+        section:
+            h2: "About"
+            p:
+                strong("emerald"); """ is a Nim library that enables you to write
+                HTML templates. It is implemented as domain-specific language that
+                can be used directly in your Nim source code via macros. Features
+                include:"""
+            ul:
+                li: strong("HTML validation"); """: emerald validates your HTML
+                    structure when it compiles your template. This validation checks
+                    for unknown, ill-placed and missing HTML tags and attributes,
+                    but does not implement the whole HTML 5 spec. It is a tool for
+                    you to discover errors early."""
+                li: strong("Mixins"); """: You can re-use parts of your template
+                    code by placing it in mixins and calling the mixin from the
+                    template. emerald is able to check the whole resulting HTML
+                    structure."""
+                li: strong("Filtering"); """: By default, emerald converts special
+                    HTML characters to their corresponding entities, but you can
+                    customize the whole filter chain and also write your own
+                    filters."""
+                li: strong("Inheritance"); """: Templates can inherit from other
+                    templates. You can define your base structure in a master
+                    template, and add content with child templates. emerald is still
+                    able to check the whole resulting HTML structure."""
+            d:
+                {. filters = pygmentize("nim") .}
+                {. preserve_whitespace = true .}
+                """
 proc templ(youAreUsingEmerald: bool) {.html_templ.} =
   html(lang = "en"):
     head:
@@ -59,10 +61,10 @@ proc templ(youAreUsingEmerald: bool) {.html_templ.} =
           templating engine which validates your
           HTML structure and relieves you from
           the ugly mess that HTML code is.
-          """"""
+              """"""
         
-            {. filters = pygmentize("html") .}
-            """
+                {. filters = pygmentize("html") .}
+                """
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
     <head>
@@ -83,15 +85,26 @@ proc templ(youAreUsingEmerald: bool) {.html_templ.} =
     </body>
 </html>"""
 
-        p:
-            """To get started with emerald, have a look at the """
-            a(href="tutorial.html", "tutorial")
-            " or learn it the hard way by reading the "
-            a(href="documentation.html", "documentation"); "."
-        
-        p:
-            "emerald has been created by "
-            a(href="https://github.com/flyx", "flyx")
-            ", is licensed under the "
-            a(href="http://www.wtfpl.net", "WTFPL"); " and can be forked on "
-            a(href="https://github.com/flyx/emerald", "Github"); "."
+            p:
+                """To get started with emerald, have a look at the """
+                a(href="tutorial.html", "tutorial")
+                " or learn it the hard way by reading the "
+                a(href="documentation.html", "documentation"); "."
+        section:
+            h2: "Contributing"
+            p:
+                """emerald is currently under development. Its language
+                definition is not finalized yet. Currently, all major features
+                are implemented, but it is a bit rough around the edges. Every
+                comment and suggestion how to improve it is welcome, just create
+                an issue on GitHub. Of course, you can also fork emerald hack it
+                yourself."""
+            h2: "Authors & License"
+            p:
+                "emerald has been created by "
+                a(href="https://github.com/flyx", "flyx")
+                ", is licensed under the "
+                a(href="http://www.wtfpl.net", "Do What the Fuck You Want to Public License")
+                " and can be forked on "
+                a(href="https://github.com/flyx/emerald", "Github"); "."
+                
